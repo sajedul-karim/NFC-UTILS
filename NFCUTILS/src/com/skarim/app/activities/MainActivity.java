7d0319b470cbafe3026096514b2bf832b040ecfe
Sajedul Karim <Sajedul Karim@SajedulKarim-PC>
2014-11-22 21:57:40 -0800

    Skarim: Full project Push to git

---
+++ NFCUTILS/src/com/skarim/app/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package com.skarim.app.activities;
+
+import com.example.nfcutils.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+}
