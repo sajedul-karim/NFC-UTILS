a71bd3662b33a312a4e4db2f20a606675feb60ff
Mahbub Hasan <Mahbub Hasan@Mahbub-PC>
2015-01-21 13:20:35 +0600

    mahbub: commit src and res file....

--- NFCUTILS/src/com/nfcutil/app/activity/MifareClassic1kActivity.java
+++ NFCUTILS/src/com/nfcutil/app/activity/MifareClassic1kActivity.java
@@ -1,14 +1,63 @@
 package com.nfcutil.app.activity;
 
+import za.co.immedia.pinnedheaderlistview.PinnedHeaderListView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
 
 import com.example.nfcutils.R;
 import com.nfcutil.app.base.NFCUtilsBase;
+import com.nfcutil.app.util.CommonValues;
 
-public class MifareClassic1kActivity extends NFCUtilsBase{
+public class MifareClassic1kActivity extends NFCUtilsBase implements OnItemClickListener{
+	TextView tvUID, tvType, tvMemory, tvPage, tvBlock;
+	PinnedHeaderListView lvMifareClassic1k;
+	
 	@Override
 	protected void onCreate(Bundle saveInstance) {
 		super.onCreate(saveInstance);
 		setContentView(R.layout.activity_mifare_classic_1k);
+		Initalization();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	private void Initalization() {
+		tvUID = (TextView) findViewById(R.id.tvUID);
+		tvType = (TextView) findViewById(R.id.tvType);
+		tvMemory = (TextView) findViewById(R.id.tvMemory);
+		tvPage = (TextView) findViewById(R.id.tvPage);
+		tvBlock = (TextView) findViewById(R.id.tvBlock);
+		lvMifareClassic1k = (PinnedHeaderListView) findViewById(R.id.lvMifareUltralLightC);
+		
+		lvMifareClassic1k.setOnItemClickListener(this);
+		setValue();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+		try{
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	private void setValue(){
+		tvUID.setText(CommonValues.getInstance().UID);
+		tvType.setText(CommonValues.getInstance().Type);
+		tvMemory.setText(CommonValues.getInstance().Memory);
+		tvPage.setText(CommonValues.getInstance().ultraLightCPageCount);
+		tvBlock.setText(CommonValues.getInstance().ultraLightCPageSize);
 	}
 }
--- NFCUTILS/src/com/nfcutil/app/activity/MifareUltralightCActivity.java
+++ NFCUTILS/src/com/nfcutil/app/activity/MifareUltralightCActivity.java
@@ -1,27 +1,63 @@
 package com.nfcutil.app.activity;
 
-import java.util.ArrayList;
-
-import android.nfc.Tag;
-import android.nfc.tech.MifareUltralight;
+import za.co.immedia.pinnedheaderlistview.PinnedHeaderListView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
 import com.example.nfcutils.R;
 import com.nfcutil.app.base.NFCUtilsBase;
-import com.nfcutil.app.entity.MifareUltraLightC;
+import com.nfcutil.app.util.CommonValues;
 
-public class MifareUltralightCActivity extends NFCUtilsBase {
-	MifareUltralight ultralight;
-	Bundle bundle;
-	Tag tag;
-	ArrayList<String> dataList = new ArrayList<String>();
-	ArrayList<MifareUltraLightC> objList = new ArrayList<MifareUltraLightC>();
-	TextView tvDataField;
+public class MifareUltralightCActivity extends NFCUtilsBase implements OnItemClickListener {
+	TextView tvUID, tvType, tvMemory, tvPage, tvBlock;
+	PinnedHeaderListView lvMifareUltralLightC;
 
 	@Override
 	protected void onCreate(Bundle saveInstance) {
 		super.onCreate(saveInstance);
 		setContentView(R.layout.activity_mifare_ultralight_c);
+		Initalization();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	private void Initalization() {
+		tvUID = (TextView) findViewById(R.id.tvUID);
+		tvType = (TextView) findViewById(R.id.tvType);
+		tvMemory = (TextView) findViewById(R.id.tvMemory);
+		tvPage = (TextView) findViewById(R.id.tvPage);
+		tvBlock = (TextView) findViewById(R.id.tvBlock);
+		lvMifareUltralLightC = (PinnedHeaderListView) findViewById(R.id.lvMifareUltralLightC);
+		
+		lvMifareUltralLightC.setOnItemClickListener(this);
+		setValue();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+		try{
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	private void setValue(){
+		tvUID.setText(CommonValues.getInstance().UID);
+		tvType.setText(CommonValues.getInstance().Type);
+		tvMemory.setText(CommonValues.getInstance().Memory);
+		tvPage.setText(CommonValues.getInstance().ultraLightCPageCount);
+		tvBlock.setText(CommonValues.getInstance().ultraLightCPageSize);
 	}
 }
