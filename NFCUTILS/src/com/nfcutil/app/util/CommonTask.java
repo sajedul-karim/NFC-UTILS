86e467633e4da8b8ccd72c38d168e7a988b87e30
Mahbub Hasan <Mahbub Hasan@Mahbub-PC>
2015-01-22 12:46:49 +0600

    mahbub: upload commontask...

--- NFCUTILS/src/com/nfcutil/app/util/CommonTask.java
+++ NFCUTILS/src/com/nfcutil/app/util/CommonTask.java
@@ -54,4 +54,13 @@ public class CommonTask {
 		}
 		return sb.toString();
 	}
+	
+	public static String hexToAscii(String hexValue) {
+	      StringBuilder output = new StringBuilder();
+	      for (int i = 0; i < hexValue.length(); i+=2) {
+	          String str = hexValue.substring(i, i+2);
+	          output.append((char)Integer.parseInt(str, 16));
+	      }
+	      return output.toString();
+	  }
 }
